db05f05578a40a8bbe344539a17a00f420016a09
SwedenLord <145365180+SwedenLord@users.noreply.github.com>
2023-09-24 00:20:41 +0400

    Add files via upload

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,6 +1,8 @@
 	
 	class MyFirstClass {
 		public static void main(String[] s) {
-			System.out.println("Hello world!!!");
+			for (int i = 0; i < s.length; i++) {
+				System.out.println(s[i]);
+			}
 			}
 	}
